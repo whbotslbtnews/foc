0fc8432bc2820489c64f257cfcdf3892e3da6656
whbotslbtnews <117298726+whbotslbtnews@users.noreply.github.com>
2022-11-09 21:14:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-//CYBER-X  DARKALPHAXTEAM ◎◎◎ https://github.com/darkalphaxteam\\
+//CYBER-X  DAs.ʟ.ʙ.ᴛ.ɴ.s.ɢ.ʙᴏᴛ ◎◎◎ https://github.com/nobot\\
 //══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
 // EXAM ඉවර උනාම ඉතුරු ටික දෙන්නම් 😓😓😓 GOOD LUCK ALL\\
 const fs = require('fs')
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94716660955'] //ur owner number
+global.ownername = "s.ʟ.ʙ.ᴛ.ɴ.s.ɢ.ʙᴏᴛ" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.socialm = "GitHub: s.ʟ.ʙ.ᴛ.ɴ.s.ɢ.ʙᴏᴛ" //ur github or insta name
+global.location = "Sri Lanka, wellawaya, koslanda" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = [''94716660955']
+global.ownertag = '94716660955' //ur tag number
+global.botnumber = '94716660955' //Add your Bot Number For Log
+global.botname = 's.ʟ.ʙ.ᴛ.ɴ.s.ɢ.ʙᴏᴛ' //ur bot name
+global.ig = "https://chat.whatsapp.com/KVqaoBMsU8OC0EsASWfNrt" //your theme url which will be displayed on whatsapp
+global.myweb = "https://chat.whatsapp.com/KVqaoBMsU8OC0EsASWfNrt" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://chat.whatsapp.com/KVqaoBMsU8OC0EsASWfNrt'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
 global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.author = "s.ʟ.ʙ.ᴛ.ɴ.s.ɢ.ʙᴏᴛ" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94716660955'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
